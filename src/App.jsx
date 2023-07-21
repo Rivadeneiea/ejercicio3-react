92f01a816bf937f53747e6eb4121da73d28d2a4a
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-20 23:52:54 -0300

    agrego componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Titulo from "./components/Titulo";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Proyecto limpiado</h1>
-      </div>
+      <Titulo></Titulo>
     </>
   );
 }
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <article>
+            <h1>Hello my friend</h1>
+        </article>
+    );
+};
+
+export default Titulo;
